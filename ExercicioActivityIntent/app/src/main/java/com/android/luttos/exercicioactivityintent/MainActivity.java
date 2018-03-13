282fd54e7ae52e19bef920b184044088b314548b
Deivison Charles <charlesluttos@iCloud.com>
2018-03-13 17:40:58 -0300

    ListView criada, nova tela criada, chamada da nova tela ok.

--- ExercicioActivityIntent/app/src/main/java/com/android/luttos/exercicioactivityintent/MainActivity.java
+++ ExercicioActivityIntent/app/src/main/java/com/android/luttos/exercicioactivityintent/MainActivity.java
@@ -1,31 +1,44 @@
 package com.android.luttos.exercicioactivityintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<String> nomes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                 *      .setAction("Action", null).show();
+                 */
+                Intent telaCadastroIntent = new Intent(MainActivity.this, Cadastro.class);
+                startActivity(telaCadastroIntent);
             }
         });
+
+        carregaLista();
+
     }
 
     @Override
@@ -48,5 +61,24 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void carregaLista(){
+        nomes = new ArrayList<>();
+        ArrayAdapter<String> RayDap = new ArrayAdapter<>(this,android.R.layout.simple_gallery_item,nomes);
+        ListView listviewAlunos= findViewById(R.id.listaAlunos);
+        listviewAlunos.setAdapter(RayDap);
+        listviewAlunos.setOnItemClickListener(listenerListaAlunos);
+    }
+
+    private OnItemClickListener listenerListaAlunos = new OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            Toast.makeText(getApplicationContext(), String.valueOf(l), Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    public void adicionarAlunos(){
+
     }
 }
