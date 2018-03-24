065fd4533daaa61620c5edbaa4a7fb2660d488b5
Deivison Charles <charlesluttos@iCloud.com>
2018-03-24 00:07:18 -0300

    Adicionado menu ao toque longo

--- ExercicioActivityIntent/app/src/main/java/com/android/luttos/exercicioactivityintent/Cadastro.java
+++ ExercicioActivityIntent/app/src/main/java/com/android/luttos/exercicioactivityintent/Cadastro.java
@@ -1,20 +1,29 @@
 package com.android.luttos.exercicioactivityintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class Cadastro extends AppCompatActivity {
-
+    EditText txtNome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        txtNome = findViewById(R.id.txtNome);
     }
 
+    public void cadastroAluno(View view){
+        Intent returnIntent = new Intent(this, MainActivity.class);
+        returnIntent.putExtra("Aluno", txtNome.getText().toString());
+        setResult(1, returnIntent);
+        finish();
+    }
 }
--- ExercicioActivityIntent/app/src/main/java/com/android/luttos/exercicioactivityintent/MainActivity.java
+++ ExercicioActivityIntent/app/src/main/java/com/android/luttos/exercicioactivityintent/MainActivity.java
@@ -5,26 +5,29 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList<String> nomes;
+    ArrayList<String> alunos;
+    ArrayAdapter<String> arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        carregaLista();
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,12 +36,9 @@ public class MainActivity extends AppCompatActivity {
                  *      .setAction("Action", null).show();
                  */
                 Intent telaCadastroIntent = new Intent(MainActivity.this, Cadastro.class);
-                startActivity(telaCadastroIntent);
+                startActivityForResult(telaCadastroIntent, 1);
             }
         });
-
-        carregaLista();
-
     }
 
     @Override
@@ -64,11 +64,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void carregaLista(){
-        nomes = new ArrayList<>();
-        ArrayAdapter<String> RayDap = new ArrayAdapter<>(this,android.R.layout.simple_gallery_item,nomes);
-        ListView listviewAlunos= findViewById(R.id.listaAlunos);
-        listviewAlunos.setAdapter(RayDap);
-        listviewAlunos.setOnItemClickListener(listenerListaAlunos);
+        alunos = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_gallery_item,alunos);
+        ListView listviewAlunos = findViewById(R.id.listaAlunos);
+        listviewAlunos.setAdapter(arrayAdapter);
+        registerForContextMenu(listviewAlunos);
     }
 
     private OnItemClickListener listenerListaAlunos = new OnItemClickListener() {
@@ -78,7 +78,22 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    public void adicionarAlunos(){
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View view, ContextMenu.ContextMenuInfo menuInfo){
+        if (view.getId() == R.id.listaAlunos){
+            MenuInflater menuInflater = getMenuInflater();
+            menuInflater.inflate(R.menu.menu_main, menu);
+        }
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1){
+            String aluno = data.getStringExtra("Aluno");
+            alunos.add(aluno);
+            Toast.makeText(getApplicationContext(), aluno+" Adicionado", Toast.LENGTH_SHORT).show();
+            arrayAdapter.notifyDataSetChanged();
+        }
     }
 }
